e3ba3192fa3b42aeb36031b68990c49f1ef06f72
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-10 14:35:24 +0900

    feat: 1. 로또 구입 금액 입력 기능 추가
    - inputUserCache(): Console.readline의 콜백함수, 입력된 값의 유효성을
      체크한 후 _userCache 멤버에 값을 지정한다.
    - invalidateCache(): 입력된 값의 유효성을 검사한다.
      isNotNumber: 숫자인지 아닌지 확인한다.
      isUnderThousand: 1000 이하인지 아닌지 확인한다.
      coubNotBeDevidedByThousand: 1000 단위인지 아닌지 확인한다.
    - returnException(): Error를 출력하고 throw를 던진다.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,66 @@ class App {
         this._userCache = userCache;
     }
 
+    play() {
+        Console.readLine('구입금액을 입력해 주세요.\n', this.inputUserCache);
+    }
+
+    inputUserCache(inputCache) {
+        const { isInvalidatedCache, errorObject } = this.invalidateCache(inputCache);
+        if (isInvalidatedCache) {
+            this.returnException(errorObject);
+            return;
+        }
+
+        const userCache = this.convertToNumber(inputCache);
+        this.setUserCache(userCache);
+    }
+
+    invalidateCache(inputCache) {
+        const checkValidation = {
+            isNotNumber: this.isNotNumber(inputCache),
+            isUnderThousand: this.isUnder(inputCache, 1000),
+            couldNotBeDevidedByThousand: this.couldNotBeDevidedBy(inputCache, 1000),
+        };
+
+        const errorType = {
+            isNotNumber: new Error('[ERROR] 숫자 이외의 문자를 입력하였습니다.'),
+            isUnderThousand: new Error('[ERROR] 금액이 부족하여 로또를 구매할 수 없습니다.'),
+            couldNotBeDevidedByThousand: new Error('[ERROR] 1,000원 단위의 금액이 아닙니다.'),
+            null: '',
+        };
+
+        const [matchError] = Object.entries(checkValidation).filter(([_, value]) => value);
+        const [errorName, _] = matchError || [null, null];
+        const validateResult = {
+            isInvalidatedCache: !!errorName,
+            errorObject: errorType[errorName],
+        };
+
+        return validateResult;
+    }
+
+    isNotNumber(source) {
+        return isNaN(+source);
+    }
+
+    couldNotBeDevidedBy(number, operand) {
+        return !!(+number % operand);
+    }
+
+    isUnder(number, threshold) {
+        return +number < threshold;
+    }
+
+    returnException(errorObject) {
+        Console.print(errorObject.message);
+        throw errorObject;
+    }
+
+    convertToNumber(source) {
+        return +source;
+    }
 }
+
 
 module.exports = App;
