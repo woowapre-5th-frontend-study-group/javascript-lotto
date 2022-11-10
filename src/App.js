939af1f414a6145f4d408c625ea0b0de6228dea9
nlom0218 <nlom0218@naver.com>
2022-11-10 19:54:57 +0900

    feat: 구입금액 입력 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.requestMoney();
+  }
+
+  requestMoney() {
+    Console.readLine('구입금액을 입력해 주세요.\n', (money) => {});
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
