4e7660a02cbc40ba5ee859ffca77c825c055767f
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-10 16:32:04 +0900

    feat: 3. 당첨 번호 입력
    - questionWinningNumber(): 당첨 번호 질문
    - inputWinningNumber(): 입력된 값에 대해 유효성 검사 후 당첨 번호 설정
    - invalidateWinningNumber(): 입력된 값에 대해 유효성 검사

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ const { Console, Random } = require('@woowacourse/mission-utils');
 class App {
     constructor() {
         this._userCache = null;
+        this._winningNumber = null;
+        this._userLotteryList = null;
+
         this.inputUserCache = this.inputUserCache.bind(this);
+        this.inputWinningNumber = this.inputWinningNumber.bind(this);
     }
 
     getUserCache() {
-        return this._userCache();
+        return this._userCache;
     }
 
     setUserCache(userCache) {
         this._userCache = userCache;
     }
 
+    getWinningNumber() {
+        return this._winningNumber;
+    }
+
+    setWinningNumber(winningNumber) {
+        this._winningNumber = winningNumber;
+    }
+
+    getUserLotteryList() {
+        return this._userLotteryList;
+    }
+
+    setUserLotteryList(userLottryList) {
+        this._userLotteryList = userLottryList;
+    }
+
     play() {
+        this.questionInputCache();
+    }
+
+    /* #region  1. 로또 구입 금액 입력 */
+
+    questionInputCache() {
         Console.readLine('구입금액을 입력해 주세요.\n', this.inputUserCache);
     }
 
@@ -27,6 +53,7 @@ class App {
 
         const userCache = this.convertToNumber(inputCache);
         this.setUserCache(userCache);
+        this.buyLotteryAndPrint();
     }
 
     invalidateCache(inputCache) {
@@ -73,6 +100,8 @@ class App {
     convertToNumber(source) {
         return +source;
     }
+    /* #endregion */
+
     /* #region  2. 구입한 로또 수량 및 번호 출력 */
     buyLotteryAndPrint() {
         this.buyLotteryTickets();
@@ -117,6 +146,72 @@ class App {
     }
     /* #endregion */
 
+    /* #region  3. 당첨 번호 입력 */
+    questionWinningNumber() {
+        Console.readLine('당첨 번호를 입력해 주세요.\n', this.inputWinningNumber);
+    }
+
+    inputWinningNumber(winningNumber) {
+        const { isInvalidatedCache, errorObject } = this.invalidateWinningNumber(winningNumber);
+        if (isInvalidatedCache) {
+            this.returnException(errorObject);
+            return;
+        }
+
+        const userWinningNumber = this.convertToNumber(winningNumber);
+        this.setWinningNumber(userWinningNumber);
+        // this.questionWinningNumber();
+    }
+
+    invalidateWinningNumber(winningNumber) {
+        const checkValidation = {
+            isNotSixLength: this.isNotLength(winningNumber, 6),
+            includeNeitherNumberNorComma: this.includeNeitherNumberNorComma(winningNumber),
+            isInNotRangeFromOneToFortyFive: this.isNotInRange(winningNumber, [1, 45]),
+            hasDuplication: this.hasDuplication(winningNumber),
+        };
+
+        const errorType = {
+            isNotSixLength: new Error('[ERROR] 6개의 번호를 입력해주세요.'),
+            includeNeitherNumberNorComma: new Error(
+                '[ERROR] 번호와 쉼표(,) 이외의 문자를 입력하였습니다.'
+            ),
+            isInNotRangeFromOneToFortyFive: new Error(
+                '[ERROR] 1부터 45 사이가 아닌 번호를 입력하였습니다.'
+            ),
+            hasDuplication: new Error('[ERROR] 번호가 중복되었습니다.'),
+            null: '',
+        };
+
+        const [matchError] = Object.entries(checkValidation).filter(([_, value]) => value);
+        const [errorName, _] = matchError || [null, null];
+        const validateResult = {
+            isInvalidatedCache: !!errorName,
+            errorObject: errorType[errorName],
+        };
+
+        return validateResult;
+    }
+
+    includeNeitherNumberNorComma(numbers) {
+        return [...numbers].some((number) => isNaN(+number) && number !== ',');
+    }
+
+    isNotInRange(numbers, [from, to]) {
+        const numberArray = numbers.split(',');
+        return numberArray.some((number) => isNaN(+number) || +number < from || +number > to);
+    }
+
+    hasDuplication(numbers) {
+        const newSetSize = new Set(numbers.split(',')).size;
+        const numbersSize = numbers.split(',').length;
+        return newSetSize !== numbersSize;
+    }
+
+    isNotLength(numbers, length) {
+        return numbers.split(',').length !== length;
+    }
+    /* #endregion */
 }
 
 
