36861231f847d15e270ee0cdcdc9def42a95b8b7
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-10 14:32:57 +0900

    feat: userCache getter/settger 지정 및 readline callback 함수 bind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
-  play() {}
+    constructor() {
+        this._userCache = null;
+        this.inputUserCache = this.inputUserCache.bind(this);
+    }
+
+    getUserCache() {
+        return this._userCache();
+    }
+
+    setUserCache(userCache) {
+        this._userCache = userCache;
+    }
+
 }
 
 module.exports = App;
