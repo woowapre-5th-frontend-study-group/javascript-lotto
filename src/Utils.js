972fd48a8477fdf5a3d839eee258825ab5f6740b
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-11 16:12:43 +0900

    feat: Utils Class 추가

---
+++ src/Utils.js
@@ -0,0 +1,65 @@
+function convertToNumber(source) {
+    return +source;
+}
+
+function convertToNumberArray(numbers) {
+    return numbers.split(',').map((number) => +number);
+}
+
+function includeNeitherNumberNorComma(numbers) {
+    return [...numbers].some((number) => isNaN(+number) && number !== ',');
+}
+
+function isNotInRange(numbers, [from, to]) {
+    let numberArray = numbers;
+
+    if (typeof numbers === 'string') {
+        numberArray = numbers.split(',');
+    }
+
+    return numberArray.some((number) => isNaN(+number) || +number < from || +number > to);
+}
+
+function hasDuplication(numbers) {
+    let numberArray = numbers;
+
+    if (typeof numbers === 'string') {
+        numberArray = numbers.split(',').map((number) => +number);
+    }
+
+    const newSetSize = new Set(numberArray).size;
+    const numbersSize = numberArray.length;
+    return newSetSize !== numbersSize;
+}
+
+function isNotLength(numbers, length) {
+    if (typeof numbers === 'object') {
+        return numbers.length !== length;
+    }
+
+    return numbers.split(',').length !== length;
+}
+
+function isNotNumber(source) {
+    return isNaN(+source);
+}
+
+function couldNotBeDevidedBy(number, operand) {
+    return !!(+number % operand);
+}
+
+function isUnder(number, threshold) {
+    return +number < threshold;
+}
+
+mdouleexports = {
+    convertToNumber,
+    convertToNumberArray,
+    includeNeitherNumberNorComma,
+    isNotInRange,
+    hasDuplication,
+    isNotLength,
+    isNotNumber,
+    couldNotBeDevidedBy,
+    isUnder,
+};
