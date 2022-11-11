e953b930db2f647cfca074cdf968e7fa510fc686
조은총 <whdmschd2@gmail.com>
2022-11-11 20:41:11 +0900

    feat: 당첨 번호 보너스 번호 일치

---
+++ src/NumbersMatch.js
@@ -0,0 +1,25 @@
+const { getIntersection } = require("./utils");
+
+class NumbersMatch {
+  userLottoNumbesMatch = [];
+  constructor(winningNumbers, bonusNumber, totalUserLottoNumbers) {
+    this.getMatchs(winningNumbers, bonusNumber, totalUserLottoNumbers);
+  }
+
+  getMatchs(winningNumbers, bonusNumber, totalUserLottoNumbers) {
+    totalUserLottoNumbers.forEach((userLottoNumbers) => {
+      const match = { winningNumberMatch: 0, isBonusNumberMatch: false };
+      match.winningNumberMatch = getIntersection(
+        winningNumbers,
+        userLottoNumbers
+      ).length;
+      if (match.winningNumberMatch < 3) {
+        return;
+      }
+      if (userLottoNumbers.includes(bonusNumber)) {
+        match.isBonusNumberMatch = true;
+      }
+      this.userLottoNumbesMatch.push(match);
+    });
+  }
+}
