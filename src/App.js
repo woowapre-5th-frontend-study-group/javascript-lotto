da47a9e4e763794175ebb83bdb8cacb8425c853a
조은총 <whdmschd2@gmail.com>
2022-11-11 18:47:04 +0900

    feat: 구입금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print("구입금액을 입력해 주세요.");
+    Console.readLine("", (purchaseAmout) => {});
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
