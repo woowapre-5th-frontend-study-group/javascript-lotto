ff130db88ecb2074f876228aa8f61bd74dd602be
조은총 <whdmschd2@gmail.com>
2022-11-14 19:10:35 +0900

    feat: 당첨 번호, 보너스 번호 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 const { Console } = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
 const UserLotto = require("./UserLotto");
 
 class App {
   userLotto;
+  winningNumbers;
+
   play() {
     Console.print("구입금액을 입력해 주세요.");
     Console.readLine("", (purchaseAmout) => {
       const userLotto = new UserLotto(purchaseAmout);
       this.userLotto = userLotto;
       this.printLottoCountNumbersMessage();
+      this.getWinningNumbers();
     });
   }
 
@@ -17,6 +21,19 @@ class App {
     this.userLotto.numbers.forEach((number) => {
       Console.print(number);
     });
+  }
+
+  getWinningNumbers() {
+    Console.print("당첨 번호를 입력해 주세요.");
+    Console.readLine("", (winningNumbers) => {
+      this.winningNumbers = new Lotto(winningNumbers.split(","));
+      this.getBonusNumber();
+    });
+  }
+
+  getBonusNumber() {
+    Console.print("보너스 번호를 입력해 주세요.");
+    Console.readLine("", (bonusNumber) => {});
   }
 }
 
