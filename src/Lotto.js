725448978a54ef07bf4895c32167b66efab3fcd7
nlom0218 <nlom0218@naver.com>
2022-12-03 19:13:21 +0900

    feat: 초기 화면

---
+++ src/Lotto.js
@@ -0,0 +1,16 @@
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error();
+    }
+  }
+}
+
+module.exports = Lotto;
