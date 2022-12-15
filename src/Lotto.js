7e396d32462e465be89290fb3cd0a2e19394a0c2
shackstack <kms1061@naver.com>
2022-12-16 00:32:59 +0900

    feat: 당첨 로또 예외처리시 예외 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,9 +11,39 @@ class Lotto {
   }
 
   validate(numbers) {
+    this.isNumeric(numbers);
+    this.isAllNumber(numbers);
+    this.isDuplicated(numbers);
+    this.isInRange(numbers);
+  }
+  isNumeric(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+  isAllNumber(numbers) {
+    numbers.forEach((number) => {
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 로또 번호는 숫자로만 이루어질 수 있습니다.");
+      }
+    });
+  }
+  isDuplicated(numbers) {
+    let set = new Set(numbers);
+    set = [...set];
+
+    if (numbers.length !== set.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되어서는 안됩니다");
+    }
+  }
+  isInRange(numbers) {
+    numbers.forEach((number) => {
+      if (number >= 1 && number <= 45) {
+        throw new Error(
+          "[ERROR] 로또 번호는 1이상 45이하의 숫자로만 이루어질 수 있습니다."
+        );
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
