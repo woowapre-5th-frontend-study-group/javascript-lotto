993f6b31de2690ba10e10f87bd2856661ba00527
shackstack <kms1061@naver.com>
2022-12-15 19:22:40 +0900

    feat: 구입금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const InputView = require("./view/InputView");
+const OutputView = require("./view/OutputView");
+
 class App {
-  play() {}
+  play() {
+    this.readPurchaseAmount();
+  }
+
+  readPurchaseAmount() {
+    InputView.readPurchaseAmount();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const InputView = {};
+const InputView = {
+  readPurchaseAmount(fn) {
+    MissionUtils.Console.readLine("구입금액을 입력해 주세요.\n", (money) => {
+      fn(money);
+    });
+  },
+};
 
 module.exports = InputView;
