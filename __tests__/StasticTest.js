5fb13b8102447438a2e9f11a9c353ea1a919f356
shackstack <kms1061@naver.com>
2022-12-16 16:38:29 +0900

    test(Bonus): 3가지 예외발생에 대한 테스트 구현

--- __tests__/StasticTest.js
+++ __tests__/StasticTest.js
@@ -1,18 +1,3 @@
 const Stastic = require("../src/Stastic");
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Stastic([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
-  });
-
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Stastic([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
-
-  // 아래에 추가 테스트 작성 가능
-});
+describe("당첨통계 클래스 테스트", () => {});
